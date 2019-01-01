04d57258ef88ba3e0c6f03b72c35f645d361f76e
Ian Huezo <ianhuezo@gmail.com>
2019-01-01 09:49:28 -0600

    Replaced the call to create a new texture each time in the movement functions with 4 new texture variables in the class that switch off for m_texture

--- PacMan/Sprite.cpp
+++ PacMan/Sprite.cpp
@@ -2,15 +2,14 @@
 
 
 Sprite::Sprite(float tileLength, const char * filePath, glm::vec2(indices))
-	:m_tileLength(tileLength), m_model(glm::mat4(1.0f)),m_texture(NULL), m_indices(indices)
+	:m_tileLength(tileLength), m_model(glm::mat4(1.0f)),m_texture(std::make_shared<Texture2D>(filePath)), m_indices(indices)
 {
 	m_model = glm::translate(m_model, glm::vec3(tileLength * 14, tileLength * 26, 0.0f));
-	m_texture = new Texture2D(filePath);
 }
 
 Sprite::~Sprite()
 {
-	delete m_texture;
+
 }
 
 void Sprite::drawSprite()
@@ -37,27 +36,31 @@ void Sprite::moveLeft()
 {
 	m_model = glm::translate(m_model, glm::vec3(-0.5, 0, 0.0f));
 }
+PacMan::~PacMan()
+{
+
+}
 //Pacman Specific movements
 void PacMan::moveUp()
 {
-	m_texture = new Texture2D("pacup.png");
+	m_texture = m_textureUp;
 	m_model = glm::translate(m_model, glm::vec3(0, -0.5, 0.0f));
 }
 
 void PacMan::moveDown()
 {
-	m_texture = new Texture2D("pacdown.png");
+	m_texture = m_textureDown;
 	m_model = glm::translate(m_model, glm::vec3(0, 0.5, 0.0f));
 }
 
 void PacMan::moveLeft()
 {
-	m_texture = new Texture2D("pacleft.png");
+	m_texture = m_textureLeft;
 	m_model = glm::translate(m_model, glm::vec3(-0.5, 0, 0.0f));
 }
 
 void PacMan::moveRight()
 {
-	m_texture = new Texture2D("pacright.png");
+	m_texture = m_textureRight;
 	m_model = glm::translate(m_model, glm::vec3(0.5, 0, 0.0f));
 }
--- PacMan/Sprite.h
+++ PacMan/Sprite.h
@@ -4,6 +4,7 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 #include "Texture2D.h"
+#include <memory>
 class Sprite
 {
 public:
@@ -19,7 +20,7 @@ protected:
 	//the position of the model
 	float m_tileLength;
 	glm::mat4 m_model;
-	Texture2D* m_texture;
+	std::shared_ptr<Texture2D> m_texture;
 	//row col compared to the tiles
 	glm::vec2 m_indices;
 };
@@ -29,9 +30,15 @@ class PacMan: public Sprite
 public:
 	PacMan(float tileLength, const char * filePath, glm::vec2(indices)) :
 		Sprite(tileLength, filePath, glm::vec2(indices)) {}
+	~PacMan();
 	virtual void moveUp();
 	virtual void moveDown();
 	virtual void moveLeft();
 	virtual void moveRight();
+private:
+	std::shared_ptr<Texture2D> m_textureUp = std::make_shared<Texture2D>("pacup.png");
+	std::shared_ptr<Texture2D> m_textureDown = std::make_shared<Texture2D>("pacdown.png");
+	std::shared_ptr<Texture2D> m_textureLeft = std::make_shared<Texture2D>("pacleft.png");
+	std::shared_ptr<Texture2D> m_textureRight = std::make_shared<Texture2D>("pacright.png");
 };
 
